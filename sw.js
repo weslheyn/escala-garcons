6d0da91498aa46480b5a57e485a2c570187bfef3
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-04-01 08:43:14 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='coco-bambu-v1';
-self.addEventListener('install',e=>e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll(['./','/escala-garcons/','/escala-garcons/index.html','/escala-garcons/manifest.json']))
-));
-self.addEventListener('fetch',e=>e.respondWith(
-  fetch(e.request).catch(()=>caches.match(e.request))
-));

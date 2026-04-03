3bf2460329efc0811a21c54c2d2bb6b54e35426f
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-04-03 06:28:54 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
-const CACHE='coco-bambu-v3';
-self.addEventListener('install',e=>e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll([
-    './',
-    './index.html',
-    './manifest.json',
-    '/escala-garcons/',
-    '/escala-garcons/index.html',
-    '/escala-garcons/manifest.json'
-  ]).catch(()=>{}))
-));
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-  ))
-));
-self.addEventListener('fetch',e=>e.respondWith(
-  fetch(e.request).catch(()=>caches.match(e.request))
-));
+const CACHE_NAME = 'frequencia-salao-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      return (
+        cached ||
+        fetch(req).catch(() => caches.match('./index.html'))
+      );
+    })
+  );
+});

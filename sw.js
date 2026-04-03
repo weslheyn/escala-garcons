228f07d090c48af4ac3d6d4eb2b5db540499fe28
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-04-03 06:33:50 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,13 @@
-const CACHE_NAME = 'frequencia-salao-v1';
-
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'frequencia-v1';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  const req = event.request;
-
-  event.respondWith(
-    caches.match(req).then(cached => {
-      return (
-        cached ||
-        fetch(req).catch(() => caches.match('./index.html'))
-      );
-    })
-  );
+  // vazio por enquanto (simples)
 });

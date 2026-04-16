afcc8836d8e6ed1a60f3a1c206fe3d72fab677d4
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-04-16 12:45:02 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='coco-bambu-v10-prod-folga-fix';
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll([
+    './',
+    './index.html',
+    './manifest.json',
+    '/escala-garcons/',
+    '/escala-garcons/index.html',
+    '/escala-garcons/manifest.json'
+  ]).catch(()=>{}))
+));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  ))
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).catch(()=>caches.match(e.request))
+));

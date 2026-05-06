c1f4377c33a2db94e4db07aacd3d9404af9d9da9
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-05-06 06:05:58 -0300

    75

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gestao-coco-bambu-v71-mobile-funil-touch';
+const CACHE='gestao-coco-bambu-v72-mobile-sem-zero';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./manifest.json','./icon.png','./index.html','./eventos.html','./eventos.css','./eventos.js','./eventos-seed.js','./eventos-firebase.js']).catch(()=>{})));

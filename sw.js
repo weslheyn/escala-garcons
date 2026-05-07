592c3675a4117c91a340a354d5f1785849c3ef00
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-05-07 02:04:42 -0300

    80

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gestao-coco-bambu-v69-funil-leads-topo';
+const CACHE='gestao-coco-bambu-v80-eventos-premium-funil-livre';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./manifest.json','./icon.png','./index.html','./eventos.html','./eventos.css','./eventos.js','./eventos-seed.js','./eventos-firebase.js']).catch(()=>{})));
